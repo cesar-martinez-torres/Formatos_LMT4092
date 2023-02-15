7764398d8068f79499ba9da73ba21a31b409d5ef
cesar-martinez-torres <cesar.martinez@udlap.mx>
2023-02-15 17:33:47 -0600

    15 febrero

--- Codigos_clase/Secc1/Unidad_2/RI9feb.m
+++ Codigos_clase/Secc1/Unidad_2/RI9feb.m
@@ -5,12 +5,13 @@ clear
 a1=5;
 a2=5;
 a3=2;
-R(1)= Link([0 2 a1 0 0]); %Generar cada una de las articulaciones 
-R(2)= Link([0 3 a2 pi/2 0]);
-R(3)= Link([0 4 a3 0 0]);
+R(1)= Link([0 0 a1 0 0]); %Generar cada una de las articulaciones 
+R(2)= Link([0 0 a2 0 0]);
+R(3)= Link([0 0 a3 0 0]);
 Robot1= SerialLink(R, 'name', 'Robot 1'); %Unir articulaciones
 Robot1.teach
 %% Evaluando la cinematica directa
 syms q1 q2 q3
-c1=[q1 q2 q3]
-conf1=Robot1.fkine(c1,'deg')
+c1g=[10 0 0]
+c1r=deg2rad(c1g)
+conf1=Robot1.fkine(c1g,'deg')
