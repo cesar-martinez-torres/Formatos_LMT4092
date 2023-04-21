dfb635fcbdfe84e916423d536a33e686adfc1515
cesar-martinez-torres <cesar.martinez@udlap.mx>
2023-04-21 15:31:41 -0500

    21 abril

--- Codigos_clase/Secc2/Unidad_3/Ejemplo_final.m
+++ Codigos_clase/Secc2/Unidad_3/Ejemplo_final.m
@@ -1,3 +1,17 @@
-%% RI 20abril
+%% RI Ejemplo final
 clear
 close all
+L1=1.4
+L2=0.95
+L3=1.25
+Le=0.4826
+A(1)= Link([0 L1 0 -pi/2 0]); %Generar cada una de las articulaciones 
+A(2)= Link([0 L2 0 pi/2 0]);%th,d,a,alpha, tipo art; 0=rot 1=pris
+A(3)= Link([-pi/2 0 0 0 1]);
+A(3).offset=L3
+A(3).qlim = [0 2];
+A(4)= Link([0 0 0 -pi/2 0]);
+A(5)= Link([0 0 0 pi/2 0]);
+A(6)= Link([0 0 0 0 0]);
+Robot= SerialLink(A, 'name', 'Final'); %Unir articulaciones
+Robot.tool=transl(0,0,Le)
