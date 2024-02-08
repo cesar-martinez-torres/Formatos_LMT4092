438be817738c9ceba118c3e40aa756f781eaf3c9
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-02-08 17:30:51 -0600

    8 febrero

--- Codigos_clase/Teoria/febrero_8.m
+++ Codigos_clase/Teoria/febrero_8.m
@@ -6,3 +6,11 @@ R(1)= Link([0 0 a1 0 0]);
 R(2)= Link([0 0 a2 0 0]);
 R(3)= Link([0 0 a3 0 0]);
 Robot1= SerialLink(R, 'name', 'Robot 1'); %Unir articulaciones
+figure
+Robot1.teach
+Qd=[15 9 30]
+Qr=deg2rad(Qd)
+Total=Robot1.fkine(Qr)
+ang=tr2rpy(Total.T,'deg')
+figure
+Robot1.plot(Qr)
