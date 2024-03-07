14e4687f9cea336d182de048b16105f5bd23634c
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-03-07 17:02:26 -0600

    7 marzo

--- Codigos_clase/Teoria/marzo_7.m
+++ Codigos_clase/Teoria/marzo_7.m
@@ -13,3 +13,9 @@ L(5)=Link([0 0 0 pi/2 0])
 L(6)=Link([0 L4 0 0 0])
 R6=SerialLink(L,'name','Antropomorfico')
 R6.teach
+%% 
+zero=[pi/4 0 pi/4 -pi/8 pi/8 pi/6]
+T0=R6.fkine(zero)
+close all
+R6.plot(zero)
+angles=tr2rpy(T0,'deg')
