9f701f1f8af651a082702e7eac066693bc4853cf
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-03-07 16:43:51 -0600

    7 marzo

---
+++ Codigos_clase/Teoria/marzo_7.m
@@ -0,0 +1,15 @@
+%Anthropomorphic 6DOF
+clear
+close all
+L1=10
+L2=5
+L3=10
+L4=3
+L(1)=Link([0 L1 0 -pi/2 0])
+L(2)=Link([0 0 L2 0 0])
+L(3)=Link([0 0 0 pi/2 0])
+L(4)=Link([0 L3 0 -pi/2 0])
+L(5)=Link([0 0 0 pi/2 0])
+L(6)=Link([0 L4 0 0 0])
+R6=SerialLink(L,'name','Antropomorfico')
+R6.teach
