85b01460491da09f27cc8aba9aade7646415669d
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-03-07 17:35:13 -0600

    7 marzo

--- Codigos_clase/Teoria/marzo_7.m
+++ Codigos_clase/Teoria/marzo_7.m
@@ -19,3 +19,16 @@ T0=R6.fkine(zero)
 close all
 R6.plot(zero)
 angles=tr2rpy(T0,'deg')
+pause()
+clear 
+close all
+L1=10
+L2=5
+L3=10
+L4=3
+L(1)=Link([0 L1 L2 0 0])
+L(2)=Link([0 0 L3 pi 0])
+L(3)=Link([0 0 0 0 1])
+L(3).qlim=[0 5]
+RRP=SerialLink(L,'name','RRP')
+RRP.teach
