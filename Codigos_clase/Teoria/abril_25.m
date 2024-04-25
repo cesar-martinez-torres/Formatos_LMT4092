d804accb6a1eff25e1a9b9ed0b103e1ec5b55099
Cesar Martinez Torres <20808@udla.fundacion.mx>
2024-04-25 17:20:23 -0600

    25 abril

--- Codigos_clase/Teoria/abril_25.m
+++ Codigos_clase/Teoria/abril_25.m
@@ -24,7 +24,21 @@ q4=jtraj(Ready(4),ApPL1(4),M)
 q5=jtraj(Ready(5),ApPL1(5),M)
 q6=jtraj(Ready(6),ApPL1(6),M)
 Q2=[q1 q2 q3 q4 q5 q6]
+%% Mov 3 ApPL1 A PL1 (ML)
+TApPL1=p560.fkine(ApPL1)
+vec=TApPL1.t
+nz=vec(3)-0.4
+t=[vec(1) vec(2) nz]
+R=TApPL1.R
+TPL1=SE3(R,t)
+qPL1=p560.ikine6s(TPL1) %% ¡¡ ES ALCANZABLE¡¡
+% Crear el movimiento
+T3=ctraj(TApPL1,TPL1,M)
+Q3=p560.ikine6s(T3)
+%% MOV 4 PL1 a ApPL1(ML)
+T4=ctraj(TPL1,TApPL1,M)
+Q4=p560.ikine6s(T4)
 %% Plotear
-tray=[Q1;Q2]
+tray=[Q1;Q2;Q3;Q4]
 figure
-p560.plot(tray,'trail','-')
+p560.plot(tray,'trail','-r')
